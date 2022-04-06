f58de54087a263948f2b21fb659036a4166f9d5c
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2022-04-06 16:13:44 +0500

    Add files via upload

--- ahsan practise/ahsan practise/9-promise-based-easyhttp/app.js
+++ ahsan practise/ahsan practise/9-promise-based-easyhttp/app.js
@@ -18,8 +18,8 @@ postBtn.addEventListener('click',function(event){
     const payload = {
         userId: 1,
         id: 1,
-        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        title: "this is title",
+        body: "this is body"
     };
     http.post('https://jsonplaceholder.typicode.com/posts',payload)
     .then(data => console.log(data))
