c6ce2046b4d10f7410fad30768731aeca0874ba9
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2022-07-21 16:17:44 +0500

    html-template full work with posts

---
+++ php/ahsan-crud/html-templates/includes/database/blog-db.php
@@ -0,0 +1,18 @@
+<?php
+
+// blog-website
+
+$servername    = "localhost";
+$username      = "root";
+$password      = "";
+$database_name = "html-template-ahsan";
+
+//connection
+$connection = mysqli_connect($servername,$username,$password,$database_name);
+// checking
+if (!$connection) {
+    die("connection failed: " . mysqli_connect_error($connection));
+    echo "not connect";
+}else{
+    echo "great";
+}
