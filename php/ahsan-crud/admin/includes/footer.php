55dea471933bc5e8c47b8ad4afeb37c7b0d565fd
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2022-08-04 14:50:54 +0500

    Delete php/ahsan-crud/admin directory

--- php/ahsan-crud/admin/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- jQuery -->
-    <script src="assets/backend/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="assets/backend/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
