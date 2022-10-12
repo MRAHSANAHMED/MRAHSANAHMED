285b291fa5cf265a0669e820d35f3fb16748c4c8
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2022-10-12 03:52:51 -0700

    laravel crud

---
+++ laravel/crud/resources/views/categories.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Categories Crud</title>
+    <!-- Latest compiled and minified CSS & JS -->
+    <link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+
+    <div class="container">
+        <h2 class="text-primary">Categories</h2>
+        <a class="btn btn-primary" href="{{ route('create_category') }}">Create Category</a>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if (count($categories) > 0)
+                    @foreach ($categories as $key => $singleCategories)
+                        <tr>
+                            <td>{{ $singleCategories->cat_id }}</td>
+                            <td>{{ $singleCategories->cat_title }}</td>
+                            <td><a class="btn btn-primary"
+                                    href="{{ route('category_edit', ['category_id' => $singleCategories->cat_id]) }}">Edit</a>
+                            </td>
+                            <td>
+                                <a class="btn btn-danger"
+                                    href="{{ route('category_delete', ['category_id' => $singleCategories->cat_id]) }}"
+                                    onclick="return confirm('Are You Sure ?')">
+                                    Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <h2>Record Not Found!</h2>
+                @endif
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ laravel/crud/resources/views/categories/create.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Categories Crud</title>
+    <!-- Latest compiled and minified CSS & JS -->
+    <link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+
+    <div class="container">
+        <h2 class="text-primary">Create Categories</h2>
+
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">
+                    {{ $error }}
+                </div>
+            @endforeach
+        @endif
+
+
+        <form action="{{ route('category_store') }}" method="POST" role="form">
+            @csrf
+            <div class="form-group">
+                <label for="">Name</label>
+                <input type="text" class="form-control" id="" placeholder="Category Title" name="cat_title">
+            </div>
+
+
+
+            <button type="submit" class="btn btn-primary">Create Category</button>
+        </form>
+
+    </div>
+</body>
+
+</html>
