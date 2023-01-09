99443459a249c95be039dd241a80fcd30e7ab085
AHSAN AHMED <97437782+MRAHSANAHMED@users.noreply.github.com>
2023-01-09 20:27:53 +0500

    rename

--- 7-reactjs/tasklistwork/src/Components/TaskForms/TaskForms.jsx
+++ 7-reactjs/tasklistwork/src/Components/TaskForms/TaskForms.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Swal from 'sweetalert2';
 import TaskList from '../TaskList/TaskList';
 
 function TaskForm() {
+  const [taskField, setTaskField] = useState();
+
+
+  const taskFieldHandler =(event) =>{
+    event.preventDefault();
+    setTaskField(event.target.value);
+  }
+
+  const formSubmitHandler =(event)=>{
+    event.preventDefault();
+    if(!taskField){
+      Swal.fire({
+        icon:"error",
+        title:"TAKE TASK",
+      });
+      return;
+    }
+    addnewTask();
+
+    setTaskField("");
+  }
   return (
     <div className="container">
       <div className="row">
@@ -10,14 +32,15 @@ function TaskForm() {
             <div className="card-content">
               <span className="card-title">Task List</span>
               <div className="row">
-                <form id="task-form" onSubmit={''}>
+                <form id="task-form" onSubmit={formSubmitHandler}>
                   <div className="input-field col s12">
+                    
                     <input
                       type="text"
                       name="task"
                       id="task"
-                      onChange={''}
-                      value={''}
+                      onChange={taskFieldHandler}
+                      value={taskField}
                     />
                     <label>new task</label>
                   </div>
--- 7-reactjs/tasklistwork/src/Components/TaskList/TaskList.jsx
+++ 7-reactjs/tasklistwork/src/Components/TaskList/TaskList.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function TaskList(props) {
-    const{} = props;
+  const {} = props;
   return (
     <div className="card-action">
-    <h5 id="task-title">Tasks</h5>
-    <div className="input-field col s12">
-      <input
-        type="text"
-        name="filter"
-        id="filter"
-        
-      />
-      <label>Filter Task</label>
-    </div>
+      <h5 id="task-title">Tasks</h5>
+      <div className="input-field col s12">
+        <input type="text" name="filter" id="filter" />
+        <label>Filter Task</label>
+      </div>
+      <ul className="collection">
+        <li className="collection-item" style={{display:"flex"}}>
+          name
+          
+            <div style={{marginLeft:"auto"}}>
+            <button>
+              <span className="material-icons">edit</span>
+            </button>
 
-    <a href="/" className="clear-tasks btn black" >
-      Clear Task
-    </a>
-  </div>
-  )
+            <button>
+            <span class="material-icons">delete</span>
+            </button>
+            </div>
+          
+        </li>
+      </ul>
+      <a href="/" className="clear-tasks btn black">
+        Clear Task
+      </a>
+    </div>
+  );
 }
 
 export default TaskList;
